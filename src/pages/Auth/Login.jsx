79eb13651d0db65595db202be7d908b600d3e173
ismaelpaul <ismaafp@gmail.com>
2022-12-04 14:50:56 +0000

    Add login page structure

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,6 +1,7 @@
-import styles from './auth/auth.module.scss';
+import styles from './auth.module.scss';
 import { FiLogIn } from 'react-icons/fi';
 import Card from '../../components/Card/Card';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
 	return (
@@ -10,6 +11,25 @@ const Login = () => {
 					<div>
 						<FiLogIn />
 					</div>
+					<h2>Log in</h2>
+					<form>
+						<div className={styles.email}>
+							<label htmlFor="email">Email</label>
+							<input type="email" placeholder="Email" required name="email" />
+						</div>
+						<div className={styles.password}>
+							<label htmlFor="password">Password</label>
+							<input
+								type="password"
+								placeholder="Password"
+								required
+								name="Password"
+							/>
+						</div>
+						<button type="submit">Log in</button>
+					</form>
+					<Link to="/forgot-password">Forgot password?</Link>
+					<Link to="/register">Register</Link>
 				</div>
 			</Card>
 		</div>
