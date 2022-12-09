1e4a75deefefd5a0a7bb00136f242d5b6bd65d96
ismaelpaul <ismaafp@gmail.com>
2022-12-09 14:12:42 +0000

    Change tags

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,20 +4,20 @@ import styles from './Header.module.scss';
 
 const Header = () => {
 	return (
-		<header>
+		<div className={styles.header}>
 			<img
 				className={styles.imageCropperHeader}
 				src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
 				alt="Rounded image representing the profile picture of the user"
 			/>
 			<div className={styles.text}>
-				<p>Welcome,</p>
 				<p>
+					Welcome,
 					<strong> Ismael</strong>
 				</p>
 			</div>
 			<IoIosArrowDown className={styles.icon} />
-		</header>
+		</div>
 	);
 };
 
