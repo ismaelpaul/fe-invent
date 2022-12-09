be7ee6b19e2c76b2c4aa68d771233d3e9c88d0cd
ismaelpaul <ismaafp@gmail.com>
2022-12-09 14:52:40 +0000

    Add Sidebar route

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Layout from './components/Layout/Layout';
+import Sidebar from './components/Sidebar/Sidebar';
 import Forgot from './pages/Auth/Forgot';
 import Login from './pages/Auth/Login';
 import Register from './pages/Auth/Register';
 import Reset from './pages/Auth/Reset';
+import Dashboard from './pages/Dashboard/Dashboard';
 import Home from './pages/Home/Home';
 
 function App() {
@@ -14,6 +18,17 @@ function App() {
 				<Route path="/register" element={<Register />} />
 				<Route path="/forgot-password" element={<Forgot />} />
 				<Route path="/reset-password/:resetToken" element={<Reset />} />
+
+				<Route
+					path="/dashboard"
+					element={
+						<Sidebar>
+							<Layout>
+								<Dashboard />
+							</Layout>
+						</Sidebar>
+					}
+				/>
 			</Routes>
 		</BrowserRouter>
 	);
