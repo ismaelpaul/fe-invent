abd6b4e08de8851e848479dfdee68299f8eb3c0a
ismaelpaul <ismaafp@gmail.com>
2022-12-09 14:54:11 +0000

    Add Footer component and initial styling

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import styles from './Footer.module.scss';
+
+const Footer = () => {
+	return (
+		<footer>
+			<p>All Rights Reserved. &copy; 2022</p>
+		</footer>
+	);
+};
+
+export default Footer;
