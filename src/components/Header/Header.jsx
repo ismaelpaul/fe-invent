2cd010c8ebbe891138b89f868fa2401f43033070
ismaelpaul <ismaafp@gmail.com>
2022-12-09 10:31:50 +0000

    Add Header component and styling

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { IoIosArrowDown } from 'react-icons/io';
+import styles from './Header.module.scss';
+
+const Header = () => {
+	return (
+		<header>
+			<img
+				className={styles.imageCropperHeader}
+				src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+				alt="Rounded image representing the profile picture of the user"
+			/>
+			<div className={styles.text}>
+				<p>Welcome,</p>
+				<p>
+					<strong> Ismael</strong>
+				</p>
+			</div>
+			<IoIosArrowDown className={styles.icon} />
+		</header>
+	);
+};
+
+export default Header;
