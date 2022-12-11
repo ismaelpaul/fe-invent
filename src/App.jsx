d5a3f3233963676fddda0ebf938a4b893edb642d
ismaelpaul <ismaafp@gmail.com>
2022-12-11 17:41:36 +0000

    Add toaster and axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
+import axios from 'axios';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
 import Layout from './components/Layout/Layout';
 import Sidebar from './components/Sidebar/Sidebar';
 import Forgot from './pages/Auth/Forgot';
@@ -8,10 +8,14 @@ import Register from './pages/Auth/Register';
 import Reset from './pages/Auth/Reset';
 import Dashboard from './pages/Dashboard/Dashboard';
 import Home from './pages/Home/Home';
+import { Toaster } from 'react-hot-toast';
+
+axios.defaults.withCredentials = true;
 
 function App() {
 	return (
 		<BrowserRouter>
+			<Toaster />
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/login" element={<Login />} />
