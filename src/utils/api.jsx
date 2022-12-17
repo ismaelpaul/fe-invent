a50a418933d037c2a9138a2bc9f4ffe6c1f3e5bb
ismaelpaul <ismaafp@gmail.com>
2022-12-17 17:42:47 +0000

    Add hr color

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -3,6 +3,12 @@ import { toast } from 'react-hot-toast';
 
 const inventApi = axios.create({
 	baseURL: 'http://localhost:9090/api',
+	headers: {
+		'Access-Control-Allow-Origin': 'http://127.0.0.1:5173',
+		'Content-Type': 'application/json',
+	},
+	withCredentials: true,
+	credentials: 'include',
 });
 
 export const registerUser = async (userData) => {
@@ -25,6 +31,34 @@ export const registerUser = async (userData) => {
 export const loginUser = async (userData) => {
 	try {
 		const response = await inventApi.post('/user/login', userData);
+
+		return response.data;
+	} catch (error) {
+		const message =
+			(error.response && error.response.data && error.response.message) ||
+			error.message ||
+			error.toString();
+		toast.error(message);
+	}
+};
+
+export const getLoginStatus = async () => {
+	try {
+		const response = await axios.get('user/loggedin');
+
+		return response.data;
+	} catch (error) {
+		const message =
+			(error.response && error.response.data && error.response.message) ||
+			error.message ||
+			error.toString();
+		toast.error(message);
+	}
+};
+
+export const getUser = async () => {
+	try {
+		const response = await inventApi.get('/user/profile');
 
 		return response.data;
 	} catch (error) {
