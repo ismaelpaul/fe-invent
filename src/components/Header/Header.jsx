3142b5a7e3b735c8d13706b247b9b79c8a8b2ea0
ismaelpaul <ismaafp@gmail.com>
2022-12-17 10:08:55 +0000

    Get user data and display on Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,38 @@
-import React from 'react';
+import { useEffect } from 'react';
 import { IoIosArrowDown } from 'react-icons/io';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+	selectUser,
+	SET_USER,
+} from '../../assets/redux/features/auth/authSlice';
+import { getUser } from '../../utils/api';
 import styles from './Header.module.scss';
 
 const Header = () => {
+	const dispatch = useDispatch();
+
+	const user = useSelector(selectUser);
+
+	useEffect(() => {
+		async function getUserData() {
+			const data = await getUser();
+
+			dispatch(SET_USER(data));
+		}
+		getUserData();
+	}, [dispatch]);
+
 	return (
 		<div className={styles.header}>
 			<img
 				className={styles.imageCropperHeader}
-				src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+				src={user.picture}
 				alt="Rounded image representing the profile picture of the user"
 			/>
 			<div className={styles.text}>
 				<p>
 					Welcome,
-					<strong> Ismael</strong>
+					<strong> {user.name}</strong>
 				</p>
 			</div>
 			<IoIosArrowDown className={styles.icon} />
