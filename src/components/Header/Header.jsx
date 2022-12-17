f51a2c1739b038e22b72b14197cc7d32981b6525
ismaelpaul <ismaafp@gmail.com>
2022-12-17 17:45:18 +0000

    Add dropdown profile

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { IoIosArrowDown } from 'react-icons/io';
 import { useDispatch, useSelector } from 'react-redux';
 import {
@@ -6,12 +6,17 @@ import {
 	SET_USER,
 } from '../../assets/redux/features/auth/authSlice';
 import { getUser } from '../../utils/api';
+import DropdownProfile from '../DropdownProfile/DropdownProfile';
 import styles from './Header.module.scss';
 
 const Header = () => {
+	const [isOpen, setIsOpen] = useState(false);
+
 	const dispatch = useDispatch();
 
 	const user = useSelector(selectUser);
+
+	const toggleDropdown = () => setIsOpen(!isOpen);
 
 	useEffect(() => {
 		async function getUserData() {
@@ -24,18 +29,31 @@ const Header = () => {
 
 	return (
 		<div className={styles.header}>
-			<img
-				className={styles.imageCropperHeader}
-				src={user.picture}
-				alt="Rounded image representing the profile picture of the user"
-			/>
+			{isOpen ? (
+				<></>
+			) : (
+				<img
+					className={styles.imageCropperHeader}
+					src={user.picture}
+					alt="Rounded image representing the profile picture of the user"
+				/>
+			)}
+
 			<div className={styles.text}>
 				<p>
 					Welcome,
 					<strong> {user.name}</strong>
 				</p>
 			</div>
-			<IoIosArrowDown className={styles.icon} />
+			<IoIosArrowDown
+				className={
+					isOpen
+						? `${styles.icon} ${styles.iconRotateDown}`
+						: `${styles.icon} ${styles.iconRotateUp}`
+				}
+				onClick={toggleDropdown}
+			/>
+			{isOpen && <DropdownProfile />}
 		</div>
 	);
 };
