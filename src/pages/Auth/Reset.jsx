f5a277d194b5c1bbe683e6ea4bf65b6f3713be6a
ismaelpaul <ismaafp@gmail.com>
2022-12-19 21:01:12 +0000

    Add reset password logic

--- src/pages/Auth/Reset.jsx
+++ src/pages/Auth/Reset.jsx
@@ -1,9 +1,53 @@
 import { RiLockPasswordLine } from 'react-icons/ri';
-import styles from './auth.module.scss';
 import Card from '../../components/Card/Card';
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { toast } from 'react-hot-toast';
+import { resetPassword } from '../../utils/api';
+import styles from './auth.module.scss';
 
 const Reset = () => {
+	const navigate = useNavigate();
+	const initialState = {
+		password: '',
+		confirmPassword: '',
+	};
+
+	const [formData, setFormData] = useState(initialState);
+
+	const { password, confirmPassword } = formData;
+	const { resetToken } = useParams();
+
+	const handleInputChange = (e) => {
+		const { name, value } = e.target;
+		setFormData({ ...formData, [name]: value });
+	};
+
+	const reset = async (e) => {
+		e.preventDefault();
+
+		if (password.length < 6) {
+			return toast.error('Passwords must be up to 6 characters.');
+		}
+
+		if (password !== confirmPassword) {
+			return toast.error('Passwords do not match.');
+		}
+
+		const userData = {
+			password,
+			confirmPassword,
+		};
+
+		try {
+			const data = await resetPassword(userData, resetToken);
+			toast.success(data.message);
+			navigate('/login');
+		} catch (error) {
+			toast.error(error);
+		}
+	};
+
 	return (
 		<div className={`container ${styles.auth}`}>
 			<Card>
@@ -13,19 +57,23 @@ const Reset = () => {
 						<h2>Reset Password</h2>
 					</div>
 
-					<form>
+					<form onSubmit={reset}>
 						<div className={styles.input}>
 							<input
 								type="password"
 								placeholder="New Password"
 								required
 								name="password"
+								value={password}
+								onChange={handleInputChange}
 							/>
 							<input
 								type="password"
 								placeholder="Confirm New Password"
 								required
-								name="password"
+								name="confirmPassword"
+								value={confirmPassword}
+								onChange={handleInputChange}
 							/>
 						</div>
 
