8cc6a5501cf4295298ab92cc24c9b26e4df03ac6
ismaelpaul <ismaafp@gmail.com>
2022-12-19 19:02:08 +0000

    Delete Log out data
    Change to a button in the DropdownProfile component

--- src/data/dropdownProfile.jsx
+++ src/data/dropdownProfile.jsx
@@ -1,17 +1,10 @@
 import { HiOutlineUser } from 'react-icons/hi';
-import { FiLogOut } from 'react-icons/fi';
 
 const dropdownProfile = [
 	{
 		title: 'Profile',
 		icon: <HiOutlineUser />,
 		path: '/profile',
-	},
-
-	{
-		title: 'Log out',
-		icon: <FiLogOut />,
-		path: '/logout',
 	},
 ];
 
