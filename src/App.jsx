91f08592c58cf4cb4dcf8aff55483b393f3722bb
ismaelpaul <ismaafp@gmail.com>
2022-12-19 19:03:06 +0000

    Add loginStatus function and remove /logout route

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import axios from 'axios';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
 import Sidebar from './components/Sidebar/Sidebar';
@@ -8,17 +7,30 @@ import Register from './pages/Auth/Register';
 import Reset from './pages/Auth/Reset';
 import Dashboard from './pages/Dashboard/Dashboard';
 import Home from './pages/Home/Home';
-import { Toaster } from 'react-hot-toast';
 import Profile from './pages/Profile/Profile';
+import { Toaster } from 'react-hot-toast';
+import { useDispatch } from 'react-redux';
+import { SET_LOGIN } from './assets/redux/features/auth/authSlice';
+import { useEffect } from 'react';
+import { getLoginStatus } from './utils/api';
 
 function App() {
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		const loginStatus = async () => {
+			const status = await getLoginStatus();
+			dispatch(SET_LOGIN(status));
+		};
+		loginStatus();
+	}, [dispatch]);
+
 	return (
 		<BrowserRouter>
 			<Toaster />
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/login" element={<Login />} />
-				<Route path="/logout" element={<Login />} />
 				<Route path="/register" element={<Register />} />
 				<Route path="/forgot-password" element={<Forgot />} />
 				<Route path="/reset-password/:resetToken" element={<Reset />} />
