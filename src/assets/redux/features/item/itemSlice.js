745626b5577eb78abf9f61a7121dc6f7f404a658
ismaelpaul <ismaafp@gmail.com>
2022-12-21 09:26:53 +0000

    Add getItems thunk

--- src/assets/redux/features/item/itemSlice.js
+++ src/assets/redux/features/item/itemSlice.js
@@ -1,6 +1,6 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { toast } from 'react-hot-toast';
-import { addNewItem } from '../../../../utils/api';
+import { addNewItem, getAllItems } from '../../../../utils/api';
 
 const initialState = {
 	item: null,
@@ -16,6 +16,21 @@ const addItem = createAsyncThunk(
 	async (newItem, thunkAPI) => {
 		try {
 			return await addNewItem(newItem);
+		} catch (error) {
+			const message =
+				(error.response && error.response.data && error.response.message) ||
+				error.message ||
+				error.toString();
+			return thunkAPI.rejectWithValue(message);
+		}
+	}
+);
+
+export const getItems = createAsyncThunk(
+	'products/getAll',
+	async (_, thunkAPI) => {
+		try {
+			return await getAllItems();
 		} catch (error) {
 			const message =
 				(error.response && error.response.data && error.response.message) ||
@@ -42,10 +57,26 @@ const itemSlice = createSlice({
 			.addCase(addItem.fulfilled, (state, action) => {
 				state.isLoading = false;
 				state.isSuccess = true;
+				state.isError = false;
 				state.items.push(action.payload);
 				toast.success('Item added succesfully.');
 			})
 			.addCase(addItem.rejected, (state, action) => {
+				state.isLoading = false;
+				state.isError = true;
+				state.message = action.payload;
+				toast.error(action.payload);
+			})
+			.addCase(getItems.pending, (state) => {
+				state.isLoading = true;
+			})
+			.addCase(getItems.fulfilled, (state, action) => {
+				state.isLoading = false;
+				state.isSuccess = true;
+				state.isError = false;
+				state.items = action.payload;
+			})
+			.addCase(getItems.rejected, (state, action) => {
 				state.isLoading = false;
 				state.isError = true;
 				state.message = action.payload;
