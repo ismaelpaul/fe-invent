d985f812583ed78c9dd16d19c43cc24242050cfa
ismaelpaul <ismaafp@gmail.com>
2022-12-28 20:33:32 +0000

    Add filterReducer

--- src/assets/redux/store.js
+++ src/assets/redux/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './features/auth/authSlice';
 import itemReducer from './features/item/itemSlice';
 import sidebarReducer from './features/sidebar/sidebarSlice';
+import filterReducer from './features/item/filterSlice';
 
 export const store = configureStore({
 	reducer: {
 		auth: authReducer,
 		item: itemReducer,
 		sidebar: sidebarReducer,
+		filter: filterReducer,
 	},
 });
