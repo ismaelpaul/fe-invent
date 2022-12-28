e183f6cd71ff99f4bbc2ba20a6793d705a599146
ismaelpaul <ismaafp@gmail.com>
2022-12-28 18:47:07 +0000

    Add AddItemModal and ItemsStats components

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,12 +1,45 @@
-import React from 'react';
+import { useEffect } from 'react';
+import { toast } from 'react-hot-toast';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectIsLoggedIn } from '../../assets/redux/features/auth/authSlice';
+import {
+	getItems,
+	selectIsOpenAddItemModal,
+} from '../../assets/redux/features/item/itemSlice';
+import AddItemModal from '../../components/Items/AddItemModal/AddItemModal';
+import ItemsList from '../../components/Items/ItemsList/ItemsList';
+import ItemsStats from '../../components/Items/ItemsStats/ItemsStats';
 import useRedirectLoggedOutUser from '../../customHook/useRedirectLoggedOutUser';
 
 const Dashboard = () => {
 	useRedirectLoggedOutUser('/login');
 
+	const dispatch = useDispatch();
+
+	const isLoggedIn = useSelector(selectIsLoggedIn);
+
+	const isOpen = useSelector(selectIsOpenAddItemModal);
+
+	const { items, isLoading, isError, message } = useSelector(
+		(state) => state.item
+	);
+
+	useEffect(() => {
+		if (isLoggedIn) {
+			dispatch(getItems());
+		}
+
+		if (isError) {
+			toast.error(message);
+		}
+	}, [isLoggedIn, isError]);
+
 	return (
 		<div>
-			<h2>Dashboard</h2>
+			{isOpen ? <AddItemModal /> : null}
+
+			<ItemsStats items={items} />
+			<ItemsList />
 		</div>
 	);
 };
