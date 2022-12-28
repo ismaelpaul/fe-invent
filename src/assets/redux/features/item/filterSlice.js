812ddccbb88b40211650ce28ac77b0e5e4e3c47c
ismaelpaul <ismaafp@gmail.com>
2022-12-28 20:34:35 +0000

    Add filterSlice
    This was created to filter items displayed from the search bar

---
+++ src/assets/redux/features/item/filterSlice.js
@@ -0,0 +1,29 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+	filteredItems: [],
+};
+
+const filterSlice = createSlice({
+	name: 'filter',
+	initialState,
+	reducers: {
+		FILTER_ITEMS(state, action) {
+			const { items, search } = action.payload;
+			const tempItems = items.filter((item) => {
+				return (
+					item.name.toLowerCase().includes(search.toLowerCase()) ||
+					item.category.toLowerCase().includes(search.toLowerCase()) ||
+					item.price.includes(search)
+				);
+			});
+			state.filteredItems = tempItems;
+		},
+	},
+});
+
+export const { FILTER_ITEMS } = filterSlice.actions;
+
+export const selectFilteredItems = (state) => state.filter.filteredItems;
+
+export default filterSlice.reducer;
