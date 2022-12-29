6a2a534100ba87e514774284cf9c0785e5ee6d3b
ismaelpaul <ismaafp@gmail.com>
2022-12-29 21:24:35 +0000

    Add values

--- src/components/Items/ItemsStats/ItemsStats.jsx
+++ src/components/Items/ItemsStats/ItemsStats.jsx
@@ -1,13 +1,31 @@
+import { useEffect } from 'react';
 import { BsCart, BsCartX } from 'react-icons/bs';
 import { MdAttachMoney } from 'react-icons/md';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+	CALC_OUT_OF_STOCK,
+	CALC_STORE_VALUE,
+	selectItemsOutOfStock,
+	selectTotalStoreValue,
+} from '../../../assets/redux/features/item/itemSlice';
+import { formatTotalStoreValue } from '../../../utils/utils';
 import ItemsInfoCard from '../../Cards/ItemsInfoCard/ItemsInfoCard';
 import styles from './ItemsStats.module.scss';
 
 const cartIcon = <BsCart color="#6D1BE7" />;
-const outOfStock = <BsCartX color="#FA3B3B" />;
+const outOfStockIcon = <BsCartX color="#FA3B3B" />;
 const storeValueIcon = <MdAttachMoney color="#009764" />;
 
 const ItemsStats = ({ items }) => {
+	const dispatch = useDispatch();
+	const totalStoreValue = useSelector(selectTotalStoreValue);
+	const itemsOutOfStock = useSelector(selectItemsOutOfStock);
+
+	useEffect(() => {
+		dispatch(CALC_STORE_VALUE(items));
+		dispatch(CALC_OUT_OF_STOCK(items));
+	}, [items]);
+
 	return (
 		<div className={styles.itemsStats__container}>
 			<h3>Items Stats</h3>
@@ -22,13 +40,13 @@ const ItemsStats = ({ items }) => {
 					iconBackgroundColor={styles.icon2}
 					icon={storeValueIcon}
 					title={'Total Store Value'}
-					count={0}
+					count={`£ ${formatTotalStoreValue(totalStoreValue.toFixed(2))}`}
 				/>
 				<ItemsInfoCard
 					iconBackgroundColor={styles.icon3}
-					icon={outOfStock}
+					icon={outOfStockIcon}
 					title={'Items Out of Stock'}
-					count={0}
+					count={itemsOutOfStock}
 				/>
 			</div>
 		</div>
