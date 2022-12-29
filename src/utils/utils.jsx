c31b89dac6f58acec2a05a0e950fa85d10f39b92
ismaelpaul <ismaafp@gmail.com>
2022-12-29 10:43:08 +0000

    Add shortenText and formatTotalStoreValue functions

--- src/utils/utils.jsx
+++ src/utils/utils.jsx
@@ -3,3 +3,15 @@ export const validateEmail = (email) => {
 		/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 	);
 };
+
+export const shortenText = (text, n) => {
+	if (text.length > n) {
+		const shortenedText = text.substring(0, n).concat('...');
+		return shortenedText;
+	}
+	return text;
+};
+
+export const formatTotalStoreValue = (value) => {
+	return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+};
