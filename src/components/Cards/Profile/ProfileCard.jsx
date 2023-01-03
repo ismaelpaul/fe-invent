24a704d0b31bacc8da0e4fc0dc3818d1182c6b56
ismaelpaul <ismaafp@gmail.com>
2023-01-03 08:01:25 +0000

    Change styling

--- src/components/Cards/Profile/ProfileCard.jsx
+++ src/components/Cards/Profile/ProfileCard.jsx
@@ -1,9 +1,7 @@
 import styles from './ProfileCard.module.scss';
 
-const ProfileCard = ({ children, cardClass }) => {
-	return (
-		<div className={`${styles.profile__card} ${cardClass}`}>{children}</div>
-	);
+const ProfileCard = ({ children }) => {
+	return <div className={styles.profile__card}>{children}</div>;
 };
 
 export default ProfileCard;
