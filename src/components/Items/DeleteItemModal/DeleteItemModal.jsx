b1aa4a5b34604dd6e465e184e79e123726cf43c9
ismaelpaul <ismaafp@gmail.com>
2023-01-07 19:55:09 +0000

    Add DeleteItemModal component and styling

---
+++ src/components/Items/DeleteItemModal/DeleteItemModal.jsx
@@ -0,0 +1,64 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { selectIsSidebarOpen } from '../../../assets/redux/features/sidebar/sidebarSlice';
+import { GrFormClose } from 'react-icons/gr';
+import '../../../styles/buttons.scss';
+import styles from './DeleteItemModal.module.scss';
+import {
+	deleteSingleItem,
+	getItems,
+	selectItemID,
+	SET_DELETE_ITEM_MODAL,
+} from '../../../assets/redux/features/item/itemSlice';
+
+const DeleteItemModal = () => {
+	const isSidebarOpen = useSelector(selectIsSidebarOpen);
+	const itemID = useSelector(selectItemID);
+
+	const dispatch = useDispatch();
+
+	const handleDeleteItem = async (itemID) => {
+		await dispatch(deleteSingleItem(itemID));
+		await dispatch(getItems());
+		await dispatch(SET_DELETE_ITEM_MODAL(false));
+	};
+	return (
+		<div
+			className={
+				isSidebarOpen
+					? `${styles.delete__modal__background} ${styles.modal__sidebarOpen}`
+					: `${styles.delete__modal__background} ${styles.modal__sidebarClose}`
+			}
+		>
+			<div className={styles.delete__modal__container}>
+				<div className={styles.delete__modal__header}>
+					<h3>Delete item</h3>
+					<div className={styles.delete__modal__close}>
+						<GrFormClose
+							onClick={() => dispatch(SET_DELETE_ITEM_MODAL(false))}
+						/>
+					</div>
+				</div>
+				<div className={styles.delete__modal__text}>
+					<p>Are you sure that you want to delete this item?</p>
+					<p>This is a non-reversible action.</p>
+				</div>
+				<div className={styles.delete__modal__buttons}>
+					<button
+						className="secondary-button"
+						onClick={() => dispatch(SET_DELETE_ITEM_MODAL(false))}
+					>
+						Cancel
+					</button>
+					<button
+						className="primary-button-delete"
+						onClick={() => handleDeleteItem(itemID)}
+					>
+						Delete item
+					</button>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default DeleteItemModal;
