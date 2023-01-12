58600465d7fbed40f5818d54c941e78b20c5cc44
ismaelpaul <ismaafp@gmail.com>
2023-01-12 11:17:51 +0000

    Add home page and styling

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,32 +1,46 @@
 import { Link } from 'react-router-dom';
-import {
-	ShowOnLogin,
-	ShowOnLogout,
-} from '../../components/protect/hiddenlinks';
+import HomeNav from '../../components/Nav/HomeNav/HomeNav';
+import heroImg from '../../assets/inv-img.png';
+import styles from './Home.module.scss';
+import '../../styles/buttons.scss';
 
 const Home = () => {
 	return (
-		<div>
-			<ul>
-				<ShowOnLogout>
-					<li>
-						<Link to="/register">Register</Link>
-					</li>
+		<div className={styles.home}>
+			<nav>
+				<HomeNav />
+			</nav>
+			<section className={styles.home__container}>
+				<header className={styles.home__hero_text}>
+					<h2>Inventory {'&'} Stock Management Solution</h2>
+					<p>
+						Inventory system to control and manage proucts in the warehouse in
+						real timeand integrated to make it easier to develop your business.
+					</p>
 
-					<li>
-						<button>
-							<Link to="/login">Log in</Link>
-						</button>
-					</li>
-				</ShowOnLogout>
-				<ShowOnLogin>
-					<li>
-						<button>
-							<Link to="/dashboard">Dashboard</Link>
-						</button>
-					</li>
-				</ShowOnLogin>
-			</ul>
+					<button className="secondary-button-home">
+						<Link to="/dashboard">Free Trial 1 Month</Link>
+					</button>
+
+					<div className={styles.home__numbers}>
+						<NumberText num="14K" text="Brand Owners" />
+						<NumberText num="23K" text="Active Users" />
+						<NumberText num="500+" text="Partners" />
+					</div>
+				</header>
+				<div className={styles.home__hero__img}>
+					<img src={heroImg} alt="Inventory" />
+				</div>
+			</section>
+		</div>
+	);
+};
+
+const NumberText = ({ num, text }) => {
+	return (
+		<div className={styles.number__text}>
+			<h3>{num}</h3>
+			<p>{text}</p>
 		</div>
 	);
 };
