adb55cdd28a65fd7154e4d1b4f850a4336d3d43d
ismaelpaul <ismaafp@gmail.com>
2023-01-14 17:39:24 +0000

    Change for reusable Card component

--- src/components/Cards/Auth/AuthCard.jsx
+++
@@ -1,7 +0,0 @@
-import styles from './AuthCard.module.scss';
-
-const AuthCard = ({ children, cardClass }) => {
-	return <div className={`${styles.card} ${cardClass}`}>{children}</div>;
-};
-
-export default AuthCard;
--- src/components/Cards/Profile/ProfileCard.jsx
+++
@@ -1,7 +0,0 @@
-import styles from './ProfileCard.module.scss';
-
-const ProfileCard = ({ children }) => {
-	return <div className={styles.profile__card}>{children}</div>;
-};
-
-export default ProfileCard;
